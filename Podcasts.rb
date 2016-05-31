61b46e70f5cb4628aa7213b34b0a6f579f032798
stevensona <adamstevenson121@gmail.com>
2016-05-30 23:33:26 -0400

    initial commit

---
+++ Podcasts.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+require 'open-uri'
+
+module Podcasts
+  class Episode
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :url
+    attr_accessor :image
+    attr_accessor :date
+    def initialize title:, author:, url:, image:, date:
+      @title = title
+      @author = author
+      @url = url
+      @image = image
+      @date = date
+    end
+  end
+
+  class Feed
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :image
+    attr_accessor :url
+    attr_accessor :items
+
+    def initialize url
+      @url = url
+      @items = Array.new
+
+      feed = Nokogiri::XML(open(@url))
+      @title = feed.xpath('//channel/title').text
+      @author = feed.xpath('//channel/itunes:author').text
+      @image = feed.xpath('//channel/itunes:image/@href').text
+      feed.xpath('//channel/item').each do |item|
+        @items << Episode.new(title: item.xpath('title').text,
+          author: item.xpath('itunes:author').text,
+          url: item.xpath('enclosure/@url').text,
+          image: item.xpath('itunes:image/@href').text,
+          date: item.xpath('pubDate').text)
+      end
+
+    end
+  end
+end
---
+++ PodcastsTests.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require_relative 'Podcasts.rb'
+
+class TestEpisode < Minitest::Test
+    def setup
+      @episode = Podcasts::Episode.new title: 'Hardwell On Air 200',
+        author: 'Hardwell',
+        url: 'http://podcast.djhardwell.com/HardwellOnAir200.m4a',
+        image: 'http://podcast.djhardwell.com/hoa200.jpg',
+        date: 'Fri, 16 Jan 2015 21:59:59 GMT'
+    end
+
+    def test_episode_attrs
+      assert_equal 'Hardwell On Air 200', @episode.title , 'title is correct'
+      assert_equal 'Hardwell', @episode.author, 'author is correct'
+      assert_equal 'http://podcast.djhardwell.com/hoa200.jpg', @episode.image ,'image url is correct'
+      assert_equal 'http://podcast.djhardwell.com/HardwellOnAir200.m4a', @episode.url, 'feed url is correct'
+      assert_equal 'Fri, 16 Jan 2015 21:59:59 GMT', @episode.date, 'date is correct'
+    end
+end
+
+class TestPodcastHardwell < Minitest::Test
+    def setup
+      @feed = Podcasts::Feed.new 'http://podcast.djhardwell.com/podcast.xml'
+    end
+
+    def test_podcast_attrs
+      assert_equal 'Hardwell On Air Official Podcast', @feed.title , 'title is correct'
+      assert_equal 'Hardwell', @feed.author, 'author is correct'
+      assert_equal 'http://podcast.djhardwell.com/hoa2015.jpg', @feed.image ,'image url is correct'
+      assert_equal 'http://podcast.djhardwell.com/podcast.xml', @feed.url, 'feed url is correct'
+      assert @feed.items.count > 0, 'there is more than one item in feed'
+    end
+end
