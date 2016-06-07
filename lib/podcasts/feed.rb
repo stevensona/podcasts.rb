fd1ea0746b2a49c429222f785b0748040418eb1e
Adam Stevenson <adamstevenson121@gmail.com>
2016-06-07 04:57:17 +0000

    missed files

---
+++ lib/podcasts/feed.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'open-uri'
+
+module Podcasts
+  class Feed
+    attr_accessor :title, :author, :image, :url, :items
+
+    def initialize url
+      @url = url
+      @items = Array.new
+
+      feed = Nokogiri::XML(open(@url))
+      @title = feed.xpath('//channel/title').text
+      @author = feed.xpath('//channel/itunes:author').text
+      @image = feed.xpath('//channel/itunes:image/@href').text
+      feed.xpath('//channel/item').each do |item|
+        @items << Episode.new(title: item.xpath('title').text,
+          author: item.xpath('itunes:author').text,
+          url: item.xpath('enclosure/@url').text,
+          image: item.xpath('itunes:image/@href').text,
+          date: item.xpath('pubDate').text)
+      end
+
+    end
+  end
+end
